9e58c90820ed6712d58b65d166ff18dd92afa5da
Fitim Istrefi <fitim.istrefi@it2pi.com>
2021-04-01 18:47:08 +0200

    3.Services Section added

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection';
-
+import ServicesSection from '../components/ServicesSection';
 const Aboutus = () =>
 {
     return(
+        <>
         <AboutSection />
+        <ServicesSection />
+        </>
     )
 }
 export default Aboutus;
