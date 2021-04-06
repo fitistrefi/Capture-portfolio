e4585cceb2415db831d20d83d34b747063a07d33
Fitim Istrefi <fitim.istrefi@it2pi.com>
2021-04-06 12:57:31 +0200

    style component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Aboutus from './pages/AboutUs';
 import AboutUs from './pages/AboutUs';
 
+import GlobalStyle from './components/GlobalStyled';
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
      <Aboutus />
     </div>
   );
