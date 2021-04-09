22a4ab89821ca76ddcae3e8e2984cc7c519b89af
Fitim Istrefi <fitim.istrefi@it2pi.com>
2021-04-10 01:41:49 +0200

    movie details added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AboutUs from "./pages/AboutUs";
 import OurWork from "./pages/OurWork";
 import ContactUs from "./pages/ContactUs";
 import Nav from "./components/Nav";
+import MovieDetail from "./pages/MovieDetail";
 
 //
 import { Switch, Route } from "react-router-dom";
@@ -18,8 +19,11 @@ function App() {
         <Route path="/" exact>
           <Aboutus />
         </Route>
-        <Route path="/work">
+        <Route path="/work" exact>
           <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
         </Route>
         <Route path="/contact">
           <ContactUs />
