891ce93d70e2dfa078e2e779bfb555a6b479fa3d
Fitim Istrefi <fitim.istrefi@it2pi.com>
2021-04-09 17:32:52 +0200

    add navigation bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Aboutus from './pages/AboutUs';
 import AboutUs from './pages/AboutUs';
+import Nav from './components/Nav';
 
 import GlobalStyle from './components/GlobalStyled';
 function App() {
   return (
     <div className="App">
+     
       <GlobalStyle />
+      <Nav />
      <Aboutus />
     </div>
   );
